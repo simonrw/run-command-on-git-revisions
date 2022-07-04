8b48862e7ee7031856cf51fa212be447fc696ed7
Simon Walker <s.r.walker101@googlemail.com>
2022-07-05 00:13:27 +0100

    Checkout state in current repo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use eyre::{Result, WrapErr};
 use git2::Repository;
-use rayon::prelude::*;
 use std::fmt::Display;
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 use std::process::Command;
 use structopt::StructOpt;
 
@@ -19,27 +18,20 @@ struct Opts {
     /// Path to repository (defaults to current directory)
     #[structopt(short, long)]
     path: Option<PathBuf>,
-    /// Run a single task at once
-    #[structopt(long)]
-    single: bool,
 }
 
-#[tracing::instrument(skip(start, end))]
-fn get_commits(repo_path: &Path, start: impl Display, end: impl Display) -> Result<Vec<git2::Oid>> {
+#[tracing::instrument(skip(repo, start, end))]
+fn get_commits(
+    repo: &Repository,
+    start: impl Display,
+    end: impl Display,
+) -> Result<Vec<git2::Oid>> {
     tracing::debug!(%start, %end, "getting commits");
-    let repo = Repository::open(repo_path)?;
     let mut walk = repo.revwalk()?;
     let range = format!("{start}..{end}", start = start, end = end);
     walk.set_sorting(git2::Sort::REVERSE)?;
     walk.push_range(&range).wrap_err("defining walk range")?;
     Ok(walk.map(|oid| oid.unwrap()).collect::<Vec<_>>())
-}
-
-#[derive(Debug)]
-struct ErrorDetail {
-    code: i32,
-    stderr: String,
-    oid: git2::Oid,
 }
 
 fn checkout(repo: &Repository, oid: git2::Oid) -> Result<()> {
@@ -60,26 +52,13 @@ fn main() -> Result<()> {
     tracing::trace!(?args, "parsed arguments");
 
     // configure the thread pool
-    if args.single {
-        rayon::ThreadPoolBuilder::new()
-            .num_threads(1)
-            .build_global()
-            .unwrap();
-    }
-
     let repo_path = args.path.unwrap_or_else(|| PathBuf::from("."));
+    let repo = Repository::discover(repo_path).wrap_err("finding repo")?;
 
-    let commits = get_commits(&repo_path, &args.start, &args.end).wrap_err("computing commits")?;
+    let commits = get_commits(&repo, &args.start, &args.end).wrap_err("computing commits")?;
     tracing::debug!(?commits, "got commits");
 
-    let tempdir = tempfile::tempdir()?;
-    let repo_path_str = repo_path.to_str().unwrap();
-    let error_messages = commits.into_par_iter().map(|oid| {
-
-        let clone_path = tempdir.path().join(oid.to_string());
-
-        let mut builder = git2::build::RepoBuilder::new();
-        let repo = builder.clone(&repo_path_str, clone_path.as_path()).unwrap();
+    for oid in commits {
         tracing::trace!("checking out commit");
         checkout(&repo, oid).unwrap();
         let working_dir = repo.path().join("..").canonicalize().unwrap();
@@ -91,32 +70,22 @@ fn main() -> Result<()> {
         tracing::info!("running user specified command");
         let output = Command::new("bash")
             .current_dir(&working_dir)
-            .args(&["-c", &args.command]).output().expect("spawning user command");
+            .args(&["-c", &args.command])
+            .output()
+            .expect("spawning user command");
         let code = output.status.code().unwrap_or(1);
 
         if output.status.success() {
             let stdout = String::from_utf8(output.stdout).unwrap();
             tracing::trace!(%stdout, %code, "successful exit code");
-            Ok(oid)
+            println!("Commit {:?} successful", oid);
         } else {
             let stderr = String::from_utf8(output.stderr).unwrap();
             tracing::trace!(%stderr, %code, "failed exit code");
-            Err(ErrorDetail {
-                code,
-                oid,
-                stderr,
-            })
-        }
-    }).collect::<Vec<_>>();
-
-    for result in error_messages {
-        match result {
-            Ok(oid) => println!("Commit {:?} successful", oid),
-            Err(ErrorDetail { oid, code, stderr }) => {
-                eprintln!("Commit {:?} failed with exit code {}", oid, code);
-                eprintln!("{}", stderr);
-            }
+            eprintln!("Commit {:?} failed with exit code {}", oid, code);
+            eprintln!("{}", stderr);
         }
     }
+
     Ok(())
 }
